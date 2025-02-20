0e4fd215af75a009e5e964460a34514d281829b3
akellataken <havrylchenko.d@gmail.com>
2025-02-20 19:43:00 +0100

    Added next and saved rendering. Implemented falling physics.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const CELL_SIZE = 20; // px
-const CANVAS_WIDTH = CELL_SIZE * 10 + 9;
-const CANVAS_HEIGHT = CELL_SIZE * 20 + 19;
+const gameCanvas = document.getElementById("game");
+const nextCanvas = document.getElementById("next");
+const savedCanvas = document.getElementById("saved");
+const gameContext = gameCanvas.getContext("2d");
+const nextContext = nextCanvas.getContext("2d");
+const savedContext = savedCanvas.getContext("2d");
 
-const c = document.getElementById("game");
-const context = c.getContext("2d");
+const CELL_SIZE = 20; // px
+const CANVAS_WIDTH = gameCanvas.width;
+const CANVAS_HEIGHT = gameCanvas.height;
+const NEXT_WIDTH = nextCanvas.width;
+const NEXT_HEIGHT = nextCanvas.height;
+const SAVED_WIDTH = savedCanvas.width;
+const SAVED_HEIGHT = savedCanvas.height;
 
 document.addEventListener("keydown", function (event) {
   switch (event.code) {
@@ -64,8 +72,7 @@ const Brush = {
   DARK_PURPLE: "rgb(125 0 125)",
 };
 
-
-function drawTriangle(x1, y1, x2, y2, x3, y3, brush) {
+function drawTriangle(x1, y1, x2, y2, x3, y3, brush, context) {
   context.fillStyle = brush;
   context.beginPath();
   context.moveTo(x1, y1);
@@ -75,8 +82,8 @@ function drawTriangle(x1, y1, x2, y2, x3, y3, brush) {
   context.fill();
 }
 
-function drawBrick(x, y, darkBrush, dimBrush, brush) {
-  drawTriangle(x, y, x + CELL_SIZE, y, x, y + CELL_SIZE, brush);
+function drawBrick(x, y, darkBrush, dimBrush, brush, context) {
+  drawTriangle(x, y, x + CELL_SIZE, y, x, y + CELL_SIZE, brush, context);
   drawTriangle(
     x + CELL_SIZE,
     y,
@@ -84,64 +91,139 @@ function drawBrick(x, y, darkBrush, dimBrush, brush) {
     y + CELL_SIZE,
     x,
     y + CELL_SIZE,
-    darkBrush
+    darkBrush,
+    context
   );
   context.fillStyle = dimBrush;
   context.fillRect(x + 2, y + 2, CELL_SIZE - 4, CELL_SIZE - 4);
 }
 
 const Brick = {
-  NONE: (x, y) => {
+  NONE: (x, y, context) => {
     context.fillStyle = Brush.BLACK;
     context.fillRect(x, y, CELL_SIZE, CELL_SIZE);
   },
-  GREEN: (x, y) => {
-    drawBrick(x, y, Brush.DARK_GREEN, Brush.DIM_GREEN, Brush.GREEN);
+  GREEN: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_GREEN, Brush.DIM_GREEN, Brush.GREEN, context);
   },
-  ORANGE: (x, y) => {
-    drawBrick(x, y, Brush.DARK_ORANGE, Brush.DIM_ORANGE, Brush.ORANGE);
+  ORANGE: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_ORANGE, Brush.DIM_ORANGE, Brush.ORANGE, context);
   },
-  BLUE: (x, y) => {
-    drawBrick(x, y, Brush.DARK_BLUE, Brush.DIM_BLUE, Brush.BLUE);
+  BLUE: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_BLUE, Brush.DIM_BLUE, Brush.BLUE, context);
   },
-  CYAN: (x, y) => {
-    drawBrick(x, y, Brush.DARK_CYAN, Brush.DIM_CYAN, Brush.CYAN);
+  CYAN: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_CYAN, Brush.DIM_CYAN, Brush.CYAN, context);
   },
-  RED: (x, y) => {
-    drawBrick(x, y, Brush.DARK_RED, Brush.DIM_RED, Brush.RED);
+  RED: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_RED, Brush.DIM_RED, Brush.RED, context);
   },
-  YELLOW: (x, y) => {
-    drawBrick(x, y, Brush.DARK_YELLOW, Brush.DIM_YELLOW, Brush.YELLOW);
+  YELLOW: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_YELLOW, Brush.DIM_YELLOW, Brush.YELLOW, context);
   },
-  PURPLE: (x, y) => {
-    drawBrick(x, y, Brush.DARK_PURPLE, Brush.DIM_PURPLE, Brush.PURPLE);
+  PURPLE: (x, y, context) => {
+    drawBrick(x, y, Brush.DARK_PURPLE, Brush.DIM_PURPLE, Brush.PURPLE, context);
   },
 };
 
+const Tetromino = {
+  SQUARE: {
+    color: Brick.YELLOW,
+    blocks: [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+  },
+  I: {
+    color: Brick.CYAN,
+    blocks: [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+      [3, 0],
+    ],
+  },
+  T: {
+    color: Brick.PURPLE,
+    blocks: [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+      [1, 0],
+    ],
+  },
+  S: {
+    color: Brick.GREEN,
+    blocks: [
+      [1, 1],
+      [2, 1],
+      [0, 0],
+      [1, 0],
+    ],
+  },
+  Z: {
+    color: Brick.RED,
+    blocks: [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [2, 0],
+    ],
+  },
+  L: {
+    color: Brick.ORANGE,
+    blocks: [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+  },
+  J: {
+    color: Brick.BLUE,
+    blocks: [
+      [2, 0],
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+  },
+};
 
-const field = Array.from({ length: 10 }, () =>
+function getRandomTetromino() {
+  const tetrominoKeys = Object.keys(Tetromino);
+  const randomKey =
+    tetrominoKeys[Math.floor(Math.random() * tetrominoKeys.length)];
+  const original = Tetromino[randomKey];
+
+  return {
+    color: original.color,
+    blocks: original.blocks.map((block) => [block[0], block[1]]),
+  };
+}
+
+let field = Array.from({ length: 10 }, () =>
   Array.from({ length: 20 }, () => Brick.NONE)
 );
 
-class BrickLocation {
-  constructor(brick, location) {
-    this.brick = brick;
-    this.location = location;
-  }
-}
-
-const currentTetromino = [new BrickLocation(Brick.GREEN, [4, 9])];
-const nextTetromino = []; 
-const savedTetromino = []; 
+let currentTetromino = getRandomTetromino();
+let nextTetromino = getRandomTetromino();
+let savedTetromino = [];
 let gameSpeed = 1.0;
+let canRenderGameTick = false;
+let offset = [3, -2];
+let paused = false;
 
 function canMoveLeft() {
-  for (let i = 0; i < currentTetromino.length; i++) {
-    const tetro = currentTetromino[i];
+  for (let i = 0; i < currentTetromino.blocks.length; i++) {
+    const tetro = currentTetromino.blocks[i];
     if (
-      tetro.location[0] <= 0 ||
-      tetro.location[1] < 0 ||
-      field[tetro.location[0] - 1][tetro.location[1]] !== Brick.NONE
+      tetro[0] + offset[0] <= 0 ||
+      tetro[1] + offset[1] < 0 ||
+      (field[tetro[0] + offset[0] - 1][tetro[1] + offset[1]] !== Brick.NONE &&
+        field[tetro[0] + offset[0] - 1][tetro[1] + offset[1]] !== undefined)
     ) {
       return false;
     }
@@ -150,12 +232,13 @@ function canMoveLeft() {
 }
 
 function canMoveRight() {
-  for (let i = 0; i < currentTetromino.length; i++) {
-    const tetro = currentTetromino[i];
+  for (let i = 0; i < currentTetromino.blocks.length; i++) {
+    const tetro = currentTetromino.blocks[i];
     if (
-      tetro.location[0] >= 9 ||
-      tetro.location[1] < 0 ||
-      field[tetro.location[0] + 1][tetro.location[1]] !== Brick.NONE
+      tetro[0] + offset[0] >= 9 ||
+      tetro[1] + offset[1] < 0 ||
+      (field[tetro[0] + offset[0] + 1][tetro[1] + offset[1]] !== Brick.NONE &&
+        field[tetro[0] + offset[0] + 1][tetro[1] + offset[1]] !== undefined)
     ) {
       return false;
     }
@@ -164,11 +247,12 @@ function canMoveRight() {
 }
 
 function canFall() {
-  for (let i = 0; i < currentTetromino.length; i++) {
-    const tetro = currentTetromino[i];
+  for (let i = 0; i < currentTetromino.blocks.length; i++) {
+    const tetro = currentTetromino.blocks[i];
     if (
-      tetro.location[1] >= 19 ||
-      field[tetro.location[0]][tetro.location[1] + 1] !== Brick.NONE
+      tetro[1] + offset[1] >= 19 ||
+      (field[tetro[0] + offset[0]][tetro[1] + offset[1] + 1] !== Brick.NONE &&
+        field[tetro[0] + offset[0]][tetro[1] + offset[1] + 1] !== undefined)
     ) {
       return false;
     }
@@ -176,66 +260,77 @@ function canFall() {
   return true;
 }
 
-// TODO: Remove later.
-function canRotate() {
-  for (let i = 0; i < currentTetromino.length; i++) {
-    const tetro = currentTetromino[i];
-    if (
-      tetro.location[1] <= 0 ||
-      field[tetro.location[0]][tetro.location[1] - 1] !== Brick.NONE
-    ) {
-      return false;
-    }
+function fixateTetromino() {
+  for (let i = 0; i < currentTetromino.blocks.length; i++) {
+    const tetro = currentTetromino.blocks[i];
+    const x = tetro[0] + offset[0];
+    const y = tetro[1] + offset[1];
+    field[x][y] = currentTetromino.color;
   }
-  return true;
+  currentTetromino = nextTetromino;
+  nextTetromino = getRandomTetromino();
+  offset = [4, -2];
 }
 
 function onKeyRight() {
   if (!canMoveRight()) return;
-  currentTetromino.forEach((tetro) => {
-    tetro.location = [tetro.location[0] + 1, tetro.location[1]];
-  });
+  offset = [offset[0] + 1, offset[1]];
 }
+
 function onKeyLeft() {
   if (!canMoveLeft()) return;
-  currentTetromino.forEach((tetro) => {
-    tetro.location = [tetro.location[0] - 1, tetro.location[1]];
-  });
+  offset = [offset[0] - 1, offset[1]];
 }
-function onKeyRotate() {
-  // TODO: Remake to actual rotate later.
-  if (!canRotate()) return;
-  currentTetromino.forEach((tetro) => {
-    tetro.location = [tetro.location[0], tetro.location[1] - 1];
-  });
-}
+
+function onKeyRotate() {}
+
 function onKeyFall() {
-  if (!canFall()) return;
-  currentTetromino.forEach((tetro) => {
-    tetro.location = [tetro.location[0], tetro.location[1] + 1];
-  });
+  if (!canFall()) {
+    fixateTetromino();
+  } else {
+    offset = [offset[0], offset[1] + 1];
+  }
 }
+
 function onKeyDrop() {}
-function onKeyPause() {}
-function onKeySave() {}
-function onKeyRestart() {}
 
-function clear() {
-  context.fillStyle = Brush.BLACK;
-  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+function onKeyPause() {
+  paused = !paused;
 }
 
-function drawMarks() {
+function onKeySave() {
+  const temp = currentTetromino;
+  if (savedTetromino.length != 0) {
+    currentTetromino = savedTetromino;
+  } else {
+    currentTetromino = getRandomTetromino();
+  }
+  savedTetromino = temp;
+}
+
+function onKeyRestart() {
+  offset = [4, -2];
+  field = Array.from({ length: 10 }, () =>
+    Array.from({ length: 20 }, () => Brick.NONE)
+  );
+}
+
+function clear(context, width, height) {
+  context.fillStyle = Brush.BLACK;
+  context.fillRect(0, 0, width, height);
+}
+
+function drawMarks(context, cellSize, width, height) {
   context.strokeStyle = Brush.LIGHT_GRAY;
-  for (let i = CELL_SIZE + 1; i <= CANVAS_WIDTH; i += CELL_SIZE + 1) {
+  for (let i = cellSize + 1; i <= width; i += cellSize + 1) {
     context.moveTo(i, 0);
-    context.lineTo(i, CANVAS_HEIGHT);
+    context.lineTo(i, height);
     context.stroke();
   }
 
-  for (let i = CELL_SIZE + 1; i <= CANVAS_HEIGHT; i += CELL_SIZE + 1) {
+  for (let i = cellSize + 1; i <= height; i += cellSize + 1) {
     context.moveTo(0, i);
-    context.lineTo(CANVAS_WIDTH, i);
+    context.lineTo(width, i);
     context.stroke();
   }
 }
@@ -243,36 +338,61 @@ function drawMarks() {
 function drawField() {
   for (let row = 0; row < field.length; row++) {
     for (let column = 0; column < field[row].length; column++) {
-      field[row][column](row * CELL_SIZE + row, column * CELL_SIZE + column);
+      field[row][column](
+        row * CELL_SIZE + row,
+        column * CELL_SIZE + column,
+        gameContext
+      );
     }
   }
 }
 
-function drawCurrentTetromino() {
-  currentTetromino.forEach((tetro) => {
-    if (tetro.location[1] < 0) return;
-    tetro.brick(
-      tetro.location[0] * CELL_SIZE + tetro.location[0],
-      tetro.location[1] * CELL_SIZE + tetro.location[1]
-    );
+function drawTetromino(tetromino, context, useOffset) {
+  if (tetromino.blocks === undefined || tetromino.blocks === 0) return;
+  tetromino.blocks.forEach((tetro) => {
+    if (tetro[1] + offset[1] < 0) return;
+    const x =
+      (tetro[0] + (useOffset ? offset[0] : 0)) * CELL_SIZE +
+      (tetro[0] + (useOffset ? offset[0] : 0));
+    const y =
+      (tetro[1] + (useOffset ? offset[1] : 0)) * CELL_SIZE +
+      (tetro[1] + (useOffset ? offset[1] : 0));
+    tetromino.color(x, y, context);
   });
 }
 
 async function render() {
-  clear();
-  drawMarks();
+  clear(gameContext, CANVAS_WIDTH, CANVAS_HEIGHT);
+  clear(nextContext, NEXT_WIDTH, NEXT_HEIGHT);
+  clear(savedContext, SAVED_WIDTH, SAVED_HEIGHT);
+  drawMarks(gameContext, CELL_SIZE, CANVAS_WIDTH, CANVAS_HEIGHT);
   drawField();
-  drawCurrentTetromino();
+  drawTetromino(currentTetromino, gameContext, true);
+  drawTetromino(nextTetromino, nextContext, false);
+  drawTetromino(savedTetromino, savedContext, false);
+}
+
+async function gameTickLoop() {
+  await new Promise((resolve) => {
+    canRenderGameTick = true;
+    setTimeout(resolve, 1000 / gameSpeed);
+  });
+  gameTickLoop();
 }
 
 async function gameLoop() {
   render();
-  await new Promise((resolve) => setTimeout(resolve, 1000 / gameSpeed));
+  await new Promise((resolve) => {
+    if (!paused) {
+      if (canRenderGameTick) {
+        onKeyFall();
+        canRenderGameTick = false;
+      }
+      setTimeout(resolve, 1000 / 30);
+    }
+  });
   window.requestAnimationFrame(gameLoop);
 }
 
 window.requestAnimationFrame(gameLoop);
-
-field[3][3] = Brick.BLUE;
-field[7][7] = Brick.BLUE;
-field[7][8] = Brick.BLUE;
+gameTickLoop();
